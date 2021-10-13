c8888f9f79c9fb32fa27ee4a507152ca9e46599e
Erincrying <1016158928@qq.com>
2021-10-13 21:12:11 +0800

    修改解析解实现一维线性回归

--- analyticalSolution2.py
+++ analyticalSolution2.py
@@ -1,45 +1,49 @@
-# 实现一元线性回归：梯度下降法
+# 实现一维线性回归：解析解(求w，b)
+
 import numpy as np
 import matplotlib.pyplot as plt
 
+# # 这里只是简单定义了5个值
+# x = np.array([1., 2., 3., 4., 5.])
+# y = np.array([1., 3., 2., 3., 5.])
+# 增加数据量
 # x轴数据
-x_data = np.arange(20)
+x = np.arange(20)
 # y轴数据
-y_data = np.array([0.4, 0.8, 1.1, 2.1, 2.8, 2.7, 3.5, 4.6, 5.1, 4.5, 6.0, 5.5, 6.9, 6.8, 7.6, 8.0, 8.8, 8.5, 9.5, 9.3])
-
-plt.scatter(x_data, y_data)
+y = np.array([0.4, 0.8, 1.1, 2.1, 2.8, 2.7, 3.5, 4.6, 5.1, 4.5, 6.0, 5.5, 6.9, 6.8, 7.6, 8.0, 8.8, 8.5, 9.5, 9.3])
+plt.scatter(x, y)
 plt.show()
 
-# 梯度下降
-# α: 学习曲率
-# epochs: 训练次数
-def run_gradient_descent(x_data, y_data, b, w, α, epochs):    
-    m = float(len(x_data))
-    for i in range(epochs):
-        b_grad = 0 # 损失函数对b的梯度
-        w_grad = 0 # 损失函数对w的梯度
-        for j in range(0, len(x_data)):
-            b_grad += (1/m) * ((b + w * x_data[j]) - y_data[j])
-            w_grad += (1/m) * ((b + w * x_data[j]) - y_data[j]) * x_data[j]
-        # 根据梯度和学习曲率修正截距b和斜率w
-        b -= α * b_grad
-        w -= α * w_grad
-        if i % 80 == 0:
-        	# 每80次作图一次
-            plt.scatter(x_data, y_data)
-            plt.plot(x_data, b + w * x_data,  color = 'r')
-            plt.show()
-    return b, w
+# x,y的均值
+x_mean = np.mean(x)
+y_mean = np.mean(y)
 
-# 设置初始参数
-α = 0.001 # 学习曲率
-b = 0 # 截距
-w = 0 # 斜率
-epochs = 500 # 批次数
+numerator = 0.0 # w的分子
+denominator_1 = 0.0 # w的分母的第一项
+denominator_2 = 0.0 # w的分母的第二项(不包含1/m)
+denominator = 0.0 # 分母
 
-b, w = run_gradient_descent(x_data, y_data, b, w, α, epochs)
-y_hat = w * x_data + b # 通过刚刚求解的w,b，基于每一个x所属的特征值进行预测
-# 绘制
-plt.scatter(x_data, y_data)
-plt.plot(x_data, y_hat,  color = 'r')
+b_sum = 0.0
+num = 0
+
+for x_i, y_i in zip(x, y):
+  numerator += y_i * (x_i - x_mean)
+  denominator_1 += x_i ** 2
+  denominator_2 += x_i
+  num += 1
+
+print(num)
+# 求解w
+denominator = denominator_1 - 1 / num * (denominator_2 ** 2)
+w = numerator / denominator
+# 求解b
+for x_i, y_i in zip(x, y):
+  b_sum += y_i - w * x_i
+b = 1 / num * b_sum
+
+y_hat = w * x + b # 通过刚刚求解的w,b，基于每一个x所属的特征值进行预测
+
+plt.scatter(x, y) # 通过散点图的方式把x，y绘制出来
+plt.plot(x, y_hat, color = 'r') # 绘制直线(x采用刚刚同样的x值,y采用刚刚预测的y_hat)
+plt.axis([0, 18, 0, 9]) # 规定坐标轴范围
 plt.show()
